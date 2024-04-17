8da071bb910ffba29165237b668eb393fdbeb585
Damien Wallis <damie.grobi@gmail.com>
2024-04-17 09:32:58 +0200

    Rectangle can move diagonally

--- src/at/dam/firstgame/Rectangles.java
+++ src/at/dam/firstgame/Rectangles.java
@@ -1,4 +1,46 @@
 package at.dam.firstgame;
 
-public class Rectangles {
+import org.newdawn.slick.*;
+import org.newdawn.slick.tests.AnimationTest;
+
+public class Rectangles extends BasicGame {
+    private int x;
+    private int y;
+
+
+    public Rectangles(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+        this.x = 100;
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+        this.x++;
+        this.y++;
+
+        if (this.x > 800 && this.y > 600) {
+            this.x = 50;
+            this.y = 50;
+        }
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        graphics.drawRect(this.x, this.y, 100, 100);
+        graphics.drawString("Hello World", 50, 50);
+    }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+            container.setDisplayMode(800, 600, false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
 }
