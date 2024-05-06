db1ab4fb22b32d65b2d853313eab457565586496
Damien Wallis <damie.grobi@gmail.com>
2024-05-06 14:43:13 +0200

    rectangle moves as intended

--- src/at/dam/firstgame/Rectangles.java
+++ src/at/dam/firstgame/Rectangles.java
@@ -1,13 +1,17 @@
 package at.dam.firstgame;
 
 import org.newdawn.slick.*;
-import org.newdawn.slick.tests.AnimationTest;
 
 public class Rectangles extends BasicGame {
-    private float x;
-    private float y;
-    private float speed;
+    public enum Directions {LEFT, RIGHT, UP, DOWN}
 
+    private Directions direction;
+
+    //rectangle-variables
+    private float rectX;
+    private float rectY;
+    private float rectSpeedX;
+    private float rectSpeedY;
 
     public Rectangles(String title) {
         super(title);
@@ -15,27 +19,42 @@ public class Rectangles extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        this.x = 100;
-        this.y = 100;
-        this.speed = 10.0f;
+        //rectangle-settings
+        this.rectX = 101;
+        this.rectY = 101;
+        this.rectSpeedX = 0.5f;
+        this.rectSpeedY = 0.5f;
+        this.direction = Directions.RIGHT;
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-        this.x += (float) delta / this.speed;
-
-        if (this.x > 800) {
-            this.x = 0;
+        //rectangle-movement
+        if (this.rectX >= 600 && this.direction == Directions.RIGHT) {
+            this.direction = Directions.DOWN;
+        } else if (this.rectY >= 400 && this.direction == Directions.DOWN) {
+            this.direction = Directions.LEFT;
+        } else if (this.rectX <= 100 && this.direction == Directions.LEFT) {
+            this.direction = Directions.UP;
+        } else if (this.rectY <= 100 && this.direction == Directions.UP) {
+            this.direction = Directions.RIGHT;
         }
 
-        if (this.y > 600) {
-            this.y = 100;
+        //rectangle-directions
+        if (this.direction == Directions.LEFT) {
+            this.rectX -= this.rectSpeedX / delta;
+        } else if (this.direction == Directions.RIGHT) {
+            this.rectX += this.rectSpeedX / delta;
+        } else if (this.direction == Directions.DOWN) {
+            this.rectY += this.rectSpeedY / delta;
+        } else if (this.direction == Directions.UP) {
+            this.rectY -= this.rectSpeedY / delta;
         }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawRect(this.x, this.y, 100, 100);
+        graphics.drawRect(this.rectX, this.rectY, 100, 100);
         graphics.drawString("Hello World", 50, 50);
     }
 
