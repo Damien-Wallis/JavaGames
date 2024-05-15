a9dc93789e5090ab3c19215b2074ef12f72214ee
Damien Wallis <damie.grobi@gmail.com>
2024-05-15 09:08:35 +0200

    04 finished

---
+++ src/at/dam/firstgame/Rectangle.java
@@ -0,0 +1,31 @@
+package at.dam.firstgame;
+
+import org.newdawn.slick.Graphics;
+
+public class Rectangle {
+    private enum Directions {LEFT, RIGHT, UP, DOWN}
+
+    private float x;
+    private float y;
+    private float speedX;
+    private float speedY;
+    private Directions direction;
+
+    public Rectangle(float x, float y, float speedX, float speedY) {
+        this.x = x;
+        this.y = y;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+    public void render(Graphics graphics) {
+        graphics.drawRect(this.x, this.y, 25, 50);
+    }
+
+    public void update(int delta) {
+        this.x += this.speedX / delta;
+        if (this.x >= 800) {
+            this.x = 0;
+        }
+    }
+}
