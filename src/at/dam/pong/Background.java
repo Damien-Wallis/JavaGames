ebba90f3bf9c882fbf169be1c37a71cc475ce927
Damien Wallis <damie.grobi@gmail.com>
2024-06-17 14:33:04 +0200

    started PongGame

---
+++ src/at/dam/pong/Background.java
@@ -0,0 +1,4 @@
+package at.dam.pong;
+
+public class Background {
+}
---
+++ src/at/dam/pong/PongGame.java
@@ -0,0 +1,27 @@
+package at.dam.pong;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class PongGame extends BasicGame {
+    public PongGame(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
